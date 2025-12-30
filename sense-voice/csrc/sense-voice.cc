5b22ece306119dfa4e2b4947dbbb4ec3d64b0303
HowardLai <fles56890929@gmail.com>
2025-12-30 23:53:06 +0800

    fixed debug mode

--- sense-voice/csrc/sense-voice.cc
+++ sense-voice/csrc/sense-voice.cc
@@ -681,7 +681,7 @@ int sense_voice_pcm_to_feature_with_state(struct sense_voice_context *ctx,
                            state->feature.frame_size,
                            state->feature.frame_step,
                            state->feature.n_mel,
-                           n_threads, true, cmvn, state->feature);
+                           n_threads, debug, cmvn, state->feature);
 
     state->t_feature_us = ggml_time_us() - t_start_us;
 
@@ -819,7 +819,7 @@ int sense_voice_batch_pcm_to_feature_with_state(struct sense_voice_context *ctx,
                                state->feature.frame_size,
                                state->feature.frame_step,
                                state->feature.n_mel,
-                               n_threads, false, cmvn, state->feature);
+                               n_threads, debug, cmvn, state->feature);
         state->feature.input_data.insert(state->feature.input_data.end(), state->feature.data.begin(), state->feature.data.end());
     }
 
